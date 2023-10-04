989e8be0c1f468abd950df748c42254569f42439
Huy IT <huyldph40152@fpt.edu.vn>
2023-10-04 13:11:00 +0700

    init project

---
+++ src/model/XuatXu.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ledin
+ */
+public class XuatXu {
+
+    private Integer id;
+    private String name;
+    private Boolean status;
+
+    public XuatXu() {
+    }
+
+    public XuatXu(Integer id, String name, Boolean status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        if (this.status == true) {
+            return "Đang hoạt động";
+        }
+        return "Ngừng bán";
+    }
+}
---
+++ src/service/XuatXuDao.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.XuatXu;
+import repository.JdbcHelper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ledin
+ */
+public class XuatXuDao extends SellingApplication<XuatXu, Integer> {
+
+    String insert_sql = """
+                        INSERT INTO [dbo].[XuatXu]
+                                   ([Ten]
+                                   ,[TrangThai])
+                             VALUES (?, ?)
+                        """;
+
+    String update_sql = """
+                        UPDATE [dbo].[XuatXu]
+                           SET [Ten] = ?
+                              ,[TrangThai] = ?
+                         WHERE ID = ?
+                        """;
+
+    String delete_sql = """
+                        DELETE FROM [dbo].[XuatXu]
+                              WHERE ID = ?
+                        """;
+
+    String selectById = """
+                        select * from XuatXu where ID = ?
+                        """;
+
+    String selectPaging = """
+                          select * from XuatXu
+                          order by ID
+                          offset ? rows fetch next ? rows only
+                          """;
+
+    String selectAll = """
+                       select * from XuatXu
+                       """;
+    
+    @Override
+    public void insert(XuatXu entity) {
+        JdbcHelper.update(insert_sql,
+                entity.getName(), entity.getStatus());
+    }
+
+    @Override
+    public void update(XuatXu entity) {
+        JdbcHelper.update(update_sql,
+                entity.getName(), entity.getStatus());
+    }
+
+    @Override
+    public void delete(Integer id) {
+        JdbcHelper.update(delete_sql, id);
+    }
+
+    @Override
+    public XuatXu selectById(Integer id) {
+        List<XuatXu> list = this.selectBySql(selectById, id);
+        if (list == null) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    @Override
+    protected List<XuatXu> selectBySql(String sql, Object... args) {
+        List<XuatXu> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcHelper.query(sql, args);
+            while (rs.next()) {
+                XuatXu xx = new XuatXu();
+                xx.setId(rs.getInt("ID"));
+                xx.setName(rs.getString("Ten"));
+                xx.setStatus(rs.getBoolean("TrangThai"));
+                list.add(xx);
+            }
+            rs.getStatement().getConnection().close();
+            return list;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<XuatXu> selectPaging(int page, int limit) {
+        return this.selectBySql(selectPaging, (page - 1)*limit, limit);
+    }
+    
+    public List<XuatXu> selectAll(){
+        return this.selectBySql(selectAll);
+    }
+}
